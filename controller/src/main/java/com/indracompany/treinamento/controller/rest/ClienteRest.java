e35654edd1b71c6c9c4064c00b3ee5ad997b1cad
VBPFelipe <felipe.vbpinheiro@gmail.com>
2022-03-22 23:31:15 -0300

    adiciona busca de clientes por nome

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -37,4 +37,14 @@ public class ClienteRest extends GenericCrudRest<Cliente, Long, ClienteService>{
 	}
 	
 
+	@GetMapping(value = "/buscarPorNome/{nome}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity<List<ClienteDTO>> buscarClientePorNome(@PathVariable String nome) {
+		List<ClienteDTO> dto = clienteService.buscarClientePorNome(nome);
+
+		if (dto == null) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+
+		return new ResponseEntity<>(dto, HttpStatus.OK);
+	}
 }
